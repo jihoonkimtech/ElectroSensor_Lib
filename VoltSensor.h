5c19967b510fa561546cba8b6bc7d1d4c17689a7
Jihoon Kim (Hanul) <kimwlgns4270@naver.com>
2020-02-14 01:29:17 +0900

    clear source

--- VoltSensor.h
+++ VoltSensor.h
@@ -1,43 +1,18 @@
 /*
- * -UltraSonic Distance Sensor Library-
- * 			for HC-SR04
+ * -Voltage Sensing Library-
  *
- * this header file for UltraSonic Distance Sensor Library
+ * this header file for Voltage Sensing Library
  *
  * author jihoonkimtech (jihoonkimtech@naver.com)
  *			(Republic of Korea)
  * version  V1.0.0
- * date  2020-02-12
+ * date  2020-02-13
 */
 
-#ifndef _UltraSonic_H_
-#define _UltraSonic_H_
+#ifndef _VoltSensor_H_
+#define _VoltSensor_H_
 
 #include "Arduino.h"
-
-enum {
-	FOUR_PIN = 0, THREE_PIN,
-	CM = 0, FEET, INCH, MM, METER
-};
-
-class UltraSonic{
-	public :
-		UltraSonic(int pinType, int pin_one, int pin_two = 0);
-		
-		/* return distance */
-		float mesureDist(int ms = 0, int legthType = 1);
-		
-		void distPrint(int ms = 0, int legthType = 1);
-		
-		void sensing(int legthType);
-		
-	private : 
-		int _pinType;
-		int _sigPin;
-		int _echoPin;
-		int _trigPin;
-		float _distance;
-};
 
 
 #endif
